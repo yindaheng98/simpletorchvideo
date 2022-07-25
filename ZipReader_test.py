317d6ed156021ce9b928523ff5d68f34a46c6ea1
yindaheng98 <yindaheng98@gmail.com>
2022-07-25 12:06:12 +0800

    fix

--- ZipReader_test.py
+++ ZipReader_test.py
@@ -5,6 +5,6 @@ from torchvideo.ZipReader import ZipReader
 reader = ZipReader("~/dataset/Vid4.zip")
 l = reader.list_file("GT")
 pprint(l)
-print(reader.read_file(l[0]))
 print(reader.read_file(l[-1]))
 print(reader.read_file(l[random.randint(0, len(l))]))
+print(reader.read_file(l[0]))
