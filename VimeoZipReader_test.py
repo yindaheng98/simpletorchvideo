7de6cd61387fbc470e4ebca2c405106dd31b0bf7
yindaheng98 <yindaheng98@gmail.com>
2022-07-25 16:42:47 +0800

    vimeo read zip

---
+++ VimeoZipReader_test.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+from torchvideo.vimeo.VimeoZipReader import VimeoZipReader
+
+inc = [
+    '00001/0266',
+    '00001/0268',
+    '00001/0275',
+    '00001/0278',
+    '00001/0285',
+    '00001/0287',
+    '00002/0209',
+    '00002/0235',
+    '00002/0236',
+    '00002/0238',
+    '00002/0241',
+    '00002/0243'
+
+]
+reader = VimeoZipReader("D:\\Documents\\MyPrograms\\视频数据\\vimeo_septuplet.zip", include_list=inc)
+l = reader.list_videos()
+pprint(l)
+print(reader.read_images(l[0][0:3]))
+print(reader.read_images(l[0][-3:]))
+print(reader.read_images(l[-1][0:3]))
+print(reader.read_images(l[-1][-3:]))
---
+++ torchvideo/vimeo/VimeoZipReader.py
@@ -0,0 +1,45 @@
+import numpy as np
+from torchvideo.reader import VideoReader, ZipImageReader
+from .util import *
+
+
+class VimeoZipReader(VideoReader):
+    def __init__(self, zip_path: str, include_list: [str]):
+        """Read a Vimeo dataset zip file. structure like vimeo_septuplet/sequences/<video index>/<slice index>/<image name>.png
+        :param zip_path: path to vimeo dataset zip file.
+        :param include_list: include of vimeo dataset. format like content in vimeo_septuplet/sep_trainlist.txt, <video index>/<slice index>
+        """
+        super().__init__()
+        self.reader = ZipImageReader(zip_path)
+        self.include_list = sorted([self._format_path(p) for p in include_list])
+        assert self.valid(), "Not a valid Vimeo zip"
+
+    def valid(self) -> bool:
+        if not self.reader.valid():
+            return False
+        try:
+            if self.reader.getinfo('vimeo_septuplet/sequences/') is None:
+                return False
+            for p in self.include_list:
+                if self.reader.getinfo('vimeo_septuplet/sequences/' + self._format_path(p) + '/') is None:
+                    return False
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def _format_path(path):
+        path = path.replace("\\", "/")
+        path = path[1:] if path[0] == "/" else path
+        path = path[0:-1] if path[-1] == "/" else path
+        return path
+
+    def read_images(self, paths: [str]) -> [np.ndarray]:
+        return [self.reader.read_image('vimeo_septuplet/sequences/' + self._format_path(path)) for path in paths]
+
+    def list_videos(self) -> [[str]]:
+        paths = []
+        root = "vimeo_septuplet/sequences/"
+        for include in self.include_list:
+            paths.extend([p.replace(root, '') for p in self.reader.list_images(root + include)])
+        return parse_video_list_from_image_list(paths)
