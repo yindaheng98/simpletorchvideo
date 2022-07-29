8457fdf498c51acc9a7d293ee51659cdfbbd5f36
doolachen <919550793@qq.com>
2022-07-29 21:43:45 +0800

    reading image now allows custom it's format.

--- simpletorchvideo/reader/ZipReader.py
+++ simpletorchvideo/reader/ZipReader.py
@@ -2,7 +2,30 @@ import logging
 import os
 import zipfile
 import cv2
+from cv2 import (IMREAD_COLOR, IMREAD_GRAYSCALE, IMREAD_IGNORE_ORIENTATION,
+                 IMREAD_UNCHANGED)
 import numpy as np
+
+jpeg = None
+supported_backends = ['cv2', 'pillow', 'tifffile']
+imread_flags = {
+    'color': IMREAD_COLOR,
+    'grayscale': IMREAD_GRAYSCALE,
+    'unchanged': IMREAD_UNCHANGED,
+    'color_ignore_orientation': IMREAD_IGNORE_ORIENTATION | IMREAD_COLOR,
+    'grayscale_ignore_orientation':
+    IMREAD_IGNORE_ORIENTATION | IMREAD_GRAYSCALE
+}
+imread_backend = 'cv2'
+try:
+    from PIL import Image, ImageOps
+except ImportError:
+    Image = None
+
+try:
+    import tifffile
+except ImportError:
+    tifffile = None
 
 logger = logging.getLogger('base')
 
@@ -45,6 +68,110 @@ class ZipImageReader:
             img_bytes = global_zipfiles[self.path].read(path)
         return cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
 
+    def read_image_custom(self, path: str,flag='unchanged', channel_order='bgr', backend=None):
+        """Read a file from a zip, and custom the image, ref from mmcv.imfrombytes
+        Args:
+            path of the file in zip
+            flag (str): Loading flag for images. Default: 'unchanged'.
+            channel_order (str): Order of channel, candidates are 'bgr' and 'rgb'.
+                Default: 'bgr'.
+            backend (str): The image loading backend type. Options are `cv2`,
+                `pillow`, and 'turbojpeg'. Default: None.
+            :returns buffer-like file content
+        Returns:
+            ndarray: Loaded image array.
+        """
+        path = self._format_path(path)
+        self._prepare_zip()
+        try:
+            img_bytes = global_zipfiles[self.path].read(path)
+        except zipfile.BadZipFile:
+            global_zipfiles[self.path].close()
+            logger.debug("Reopen zip file: %s" % self.path)
+            global_zipfiles[self.path] = zipfile.ZipFile(self.path, "r")
+            img_bytes = global_zipfiles[self.path].read(path)
+
+        return self._customimage_from_bytes(img_bytes,
+                flag=flag,
+                channel_order=channel_order,
+                backend=backend)
+
+    def _customimage_from_bytes(self,img_bytes,flag,channel_order,backend):
+        if backend is None:
+            backend = imread_backend
+        if backend not in supported_backends:
+            raise ValueError(f'backend: {backend} is not supported. Supported '
+            "backends are 'cv2', 'pillow', 'tifffile'")
+        if backend == 'pillow':
+            with io.BytesIO(img_bytes) as buff:
+                img = Image.open(buff)
+                img = self._pillow2array(img, flag, channel_order)
+            return img
+        elif backend == 'tifffile':
+            with io.BytesIO(img_bytes) as buff:
+                img = tifffile.imread(buff)
+            return img
+        else:
+            img_np = np.frombuffer(img_bytes, np.uint8)
+            flag = imread_flags[flag] if isinstance(flag, str) else flag
+            img = cv2.imdecode(img_np, flag)
+            if flag == IMREAD_COLOR and channel_order == 'rgb':
+                cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+            return img
+
+    def _pillow2array(img, flag='color', channel_order='bgr'):
+        """Convert a pillow image to numpy array.
+
+        Args:
+            img (:obj:`PIL.Image.Image`): The image loaded using PIL
+            flag (str): Flags specifying the color type of a loaded image,
+                candidates are 'color', 'grayscale' and 'unchanged'.
+                Default to 'color'.
+            channel_order (str): The channel order of the output image array,
+                candidates are 'bgr' and 'rgb'. Default to 'bgr'.
+
+        Returns:
+            np.ndarray: The converted numpy array
+        """
+        channel_order = channel_order.lower()
+        if channel_order not in ['rgb', 'bgr']:
+            raise ValueError('channel order must be either "rgb" or "bgr"')
+
+        if flag == 'unchanged':
+            array = np.array(img)
+            if array.ndim >= 3 and array.shape[2] >= 3:  # color image
+                array[:, :, :3] = array[:, :, (2, 1, 0)]  # RGB to BGR
+        else:
+            # Handle exif orientation tag
+            if flag in ['color', 'grayscale']:
+                img = ImageOps.exif_transpose(img)
+            # If the image mode is not 'RGB', convert it to 'RGB' first.
+            if img.mode != 'RGB':
+                if img.mode != 'LA':
+                    # Most formats except 'LA' can be directly converted to RGB
+                    img = img.convert('RGB')
+                else:
+                    # When the mode is 'LA', the default conversion will fill in
+                    #  the canvas with black, which sometimes shadows black objects
+                    #  in the foreground.
+                    #
+                    # Therefore, a random color (124, 117, 104) is used for canvas
+                    img_rgba = img.convert('RGBA')
+                    img = Image.new('RGB', img_rgba.size, (124, 117, 104))
+                    img.paste(img_rgba, mask=img_rgba.split()[3])  # 3 is alpha
+            if flag in ['color', 'color_ignore_orientation']:
+                array = np.array(img)
+                if channel_order != 'rgb':
+                    array = array[:, :, ::-1]  # RGB to BGR
+            elif flag in ['grayscale', 'grayscale_ignore_orientation']:
+                img = img.convert('L')
+                array = np.array(img)
+            else:
+                raise ValueError(
+                    'flag must be "color", "grayscale", "unchanged", '
+                    f'"color_ignore_orientation" or "grayscale_ignore_orientation"'
+                    f' but got {flag}')
+        return array
     def _prepare_zip(self):
         if self.path not in global_zipfiles or global_zipfiles[self.path] is None:
             global_zipfiles[self.path] = zipfile.ZipFile(self.path, "r")
